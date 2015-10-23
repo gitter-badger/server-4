ff19d12125c0d870967d865208e6044102e209ab
Stephan Arts <stephan@xfce.org>
2015-10-23 22:28:28 +0200

    Refactor metric view class

--- arguxserver/rest/views/metric.py
+++ arguxserver/rest/views/metric.py
@@ -15,34 +15,36 @@ class RestMetricViews:
         self.request = request
 
     @view_config(route_name='metric_1')
-    def metrics(self):
+    def metric_1_view(self):
 
         host = self.request.matchdict['host']
         item = self.request.matchdict['item']
 
         if (self.request.method == "GET"):
-            time = self.request.params.get('time', '60')
-            start_time = self.request.params.get('start_time', '-1')
-            end_time = self.request.params.get('end_time', '-1')
-            return {'fqdn': host, 'item': item, 'time': time}
+            self.metric_1_view_read(host, item)
 
         if (self.request.method == "POST"):
-            try:
-                value = self.request.json_body.get('value', None)
-            except ValueError:
-                value= None
-                return Response(
-                    status='400 Bad Request',
-                    content_type='application/json',
-                    charset='UTF-8',
-                    body='{"error": "400 Bad Request", "message": "value not specified"}')
+            self.metric_1_view_create(host, item)
 
-            h = models.DBSession.query(models.Host).filter(models.Host.name == host).first()
-            i = models.DBSession.query(models.Item).filter(models.Item.host == h).filter(models.Item.key == item)
+    def metric_1_view_read(self, host, item):
+        time = self.request.params.get('time', '60')
+        start_time = self.request.params.get('start_time', '-1')
+        end_time = self.request.params.get('end_time', '-1')
+        return {'fqdn': host, 'item': item, 'time': time}
+
+    def metric_1_view_create(self, host, item):
+        try:
+            value = self.request.json_body.get('value', None)
+        except ValueError:
+            value= None
             return Response(
-                status='201 Created',
-                content_type='application/json; charset=UTF-8')
+                status='400 Bad Request',
+                content_type='application/json',
+                charset='UTF-8',
+                body='{"error": "400 Bad Request", "message": "value not specified"}')
 
-        if (self.request.method == "POST"):
-            return {'fqdn':'POST'}
-
+        h = models.DBSession.query(models.Host).filter(models.Host.name == host).first()
+        i = models.DBSession.query(models.Item).filter(models.Item.host == h).filter(models.Item.key == item)
+        return Response(
+            status='201 Created',
+            content_type='application/json; charset=UTF-8')
