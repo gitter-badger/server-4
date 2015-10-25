a5554a2f0cc0701dcaf88568f8a25ac2ed0dbeb7
Stephan Arts <stephan@xfce.org>
2015-10-25 11:57:16 +0100

    Remove duplicate code

--- arguxserver/dao/ValuesDAO.py
+++ arguxserver/dao/ValuesDAO.py
@@ -6,44 +6,20 @@ from arguxserver.models import (
     FloatValue
     )
 
-def __pushIntValue(item, timestamp, value):
-    v = IntValue(item_id = item.id, timestamp=timestamp, value=value)
-    DBSession.add(v)
-    return
-
-def __pushFloatValue(item, ts, value):
-    v = FloatValue(item_id = item.id, timestamp=ts, value=value)
-    DBSession.add(v)
-    return
-
 # Map
-__push_value_functions = {
-    "int" : __pushIntValue,
-    "float" : __pushFloatValue
+__push_value_class = {
+    "int" : FloatValue,
+    "float" : IntValue
 }
 
 
 def pushValue(item, timestamp, value):
-    func = __push_value_functions.get(item.itemtype.name, lambda: "nothing")
-    return func(item, timestamp, value)
-
-
-def __getIntValue(item):
-    c = DBSession.query(IntValue).filter(IntValue.item_id == item.id).first()
-    return c
-
-def __getFloatValue(item):
-    c = DBSession.query(FloatValue).filter(FloatValue.item_id == item.id).first()
-    return c
-
-
-# Map
-__get_value_functions = {
-    "int" : __getIntValue,
-    "float" : __getFloatValue
-}
-
+    klass = __push_value_class.get(item.itemtype.name, lambda: "nothing")
+    i = klass(item_id = item.id, timestamp=timestamp, value=value)
+    DBSession.add(i)
+    return
 
 def getValues(item):
-    func = __get_value_functions.get(item.itemtype.name, lambda: "nothing")
-    return func(item)
+    klass = __push_value_class.get(item.itemtype.name, lambda: "nothing")
+    c = DBSession.query(klass).filter(klass.item_id == item.id).first()
+    return c
