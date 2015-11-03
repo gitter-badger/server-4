84d00fa4efc6879fc35d787927893ace44eed7cb
Stephan Arts <stephan@xfce.org>
2015-11-03 06:47:55 +0100

    Update NoteDAO

--- arguxserver/dao/NoteDAO.py
+++ arguxserver/dao/NoteDAO.py
@@ -7,4 +7,14 @@ from arguxserver.models import (
 
 
 def getNotesForHost(host):
+    #c = DBSession.query(NoteMap).filter(NoteMap.host_id == host.id)
     return None
+
+def createNote(subject, body, timestamp):
+    n = Note(subject=subject,body=body, timestamp=timestamp)
+    DBSession.add(n)
+    return n
+
+def mapNoteToHost(note, host):
+    m = NoteMap(note_id=note.id, host_id = host.id)
+    DBSession.add(m)
