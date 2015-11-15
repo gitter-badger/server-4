68b8e686716a6bb074e9b2c177eea184479ab0ad
Stephan Arts <stephan@xfce.org>
2015-11-15 15:25:40 +0100

    only run AJAX request when showing details page

--- arguxserver/static/item.js
+++ arguxserver/static/item.js
@@ -1,4 +1,5 @@
 /* globals Chart: false */
+/* globals ARGUX_BASE: false */
 /* globals ARGUX_HOST: false */
 /* globals ARGUX_ITEM: false */
 /* globals ARGUX_ITEM_ACTION: false */
@@ -60,28 +61,30 @@ var ctx = document.getElementById("myChart").getContext("2d");
 var myNewChart = new Chart(ctx, config);
 
 $(function() {
-    function doPoll() {
-        $.ajax({
-            url: ARGUX_BASE+"/rest/1.0/host/"+ARGUX_HOST+"/item/"+ARGUX_ITEM+"/values?query=a&show_date=false",
-            type: "GET",
-            dataType: "json",
-            success: function(json) {
+    if (ARGUX_ITEM_ACTION==="details") {
+        function doPoll() {
+            $.ajax({
+                url: ARGUX_BASE+"/rest/1.0/host/"+ARGUX_HOST+"/item/"+ARGUX_ITEM+"/values?query=a&show_date=false",
+                type: "GET",
+                dataType: "json",
+                success: function(json) {
 
-                var datapoints = [];
+                    var datapoints = [];
 
-                $.each(json.values, function(i, value) {
-                    datapoints.push({
-                        x: value.ts,
-                        y: value.value});
-                });
+                    $.each(json.values, function(i, value) {
+                        datapoints.push({
+                            x: value.ts,
+                            y: value.value});
+                    });
 
-                config.data.datasets[0].data = datapoints;
+                    config.data.datasets[0].data = datapoints;
 
-                myNewChart.update();
+                    myNewChart.update();
 
-                setTimeout(doPoll, 3000);
-            }
-        });
+                    setTimeout(doPoll, 3000);
+                }
+            });
+        }
+        doPoll();
     }
-    doPoll();
 });
