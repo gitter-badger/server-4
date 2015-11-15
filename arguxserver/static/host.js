41ce86b0f55e423e20e60314df0a67fef9a49ae5
Stephan Arts <stephan@xfce.org>
2015-11-15 12:45:00 +0100

    Cleanup host.js

--- arguxserver/static/host.js
+++ arguxserver/static/host.js
@@ -2,204 +2,193 @@
 /* globals ARGUX_HOST_ACTION: false */
 /* globals ARGUX_BASE: false */
 
-$(function() {
+function pollNotes() {
+    $.ajax({
+        url: ARGUX_BASE+"/rest/1.0/note?host="+ARGUX_HOST,
+        type: "GET",
+        dataType: "json",
+        success: function(json) {
 
-if (ARGUX_HOST_ACTION=='details') {
+            $('#items').empty()
+            // Build the panel contents.
+            $.each(json.notes, function(i, note) {
+                // Note timestamp is in ISO format.
+                ts = new Date(note.timestamp)
 
-}
-if (ARGUX_HOST_ACTION=='notes') {
-    $('#notes-form').submit(function(event) {
-
-        var subject = $('#note-subject').val();
-        var message = $('#note-body').val();
-
-        if (( subject !== "") &&
-            ( message !== "")) {
-            // Use JSON.stringify to properly escape the message and subject
-            // for use in a JSON message envelope
-            $.ajax({
-                type: 'POST',
-                url:  ARGUX_BASE+'/rest/1.0/note',
-                dataType: 'json',
-                data: '{'+
-                      '"host": "'+ARGUX_HOST+'",' +
-                      '"message": '+JSON.stringify(message)+',' +
-                      '"subject": '+JSON.stringify(subject)+
-                      '}'
+                $('#items').append(
+                    '<div class="panel panel-default">' +
+                    '<div class="panel-heading">' +
+                    note.subject +
+                    '</div>' +
+                    '<div class="panel-body">' +
+                    '<p>' +
+                    note.message +
+                    '</p>' +
+                    '<div class="xs">' +
+                    ts.toLocaleString() +
+                    '</div>' +
+                    '</div>' +
+                    '</div>');
             });
+            setTimeout(pollNotes, 5000);
         }
     });
+}
 
-    function doPoll() {
-        $.ajax({
-            url: ARGUX_BASE+"/rest/1.0/note?host="+ARGUX_HOST,
-            type: "GET",
-            dataType: "json",
-            success: function(json) {
+function pollMetrics() {
+    $.ajax({
+        url: ARGUX_BASE+"/rest/1.0/host/"+ARGUX_HOST+"?items=true",
+        type: "GET",
+        dataType: "json",
+        success: function(json) {
 
-                $('#items').empty()
-                // Build the panel contents.
-                $.each(json.notes, function(i, note) {
-                    // Note timestamp is in ISO format.
-                    ts = new Date(note.timestamp)
+            var categories = {}
+            var alerts     = {}
+            var collapsed  = {};
 
+            // Build the panel contents.
+            $.each(json.items, function(i, item) {
+
+                var category = 'global';
+                var item_name = item.key;
+                var item_value = 'unknown';
+                var item_time = '-';
+
+                // Pick a category
+                if (item.category !== null) { category = item.category;
+                }
+
+                // If name exists, don't use the key.
+                if (item.name !== null) {
+                    item_name = item.name;
+                }
+
+                //
+                if (item.last_ts !== undefined) {
+                    item_time = item.last_ts;
+                }
+
+                if (item.last_val !== undefined) {
+                    item_value = item.last_val;
+                }
+
+                /* Initialise alerts to '' */
+                if (alerts.hasOwnProperty(category) === false) {
+                    alerts[category] = '';
+                }
+                var item_base_url = 
+                    ARGUX_BASE +
+                    '/host/' +
+                    ARGUX_HOST +
+                    '/item/' +
+                    item.key
+
+                categories[category]+='<tr>' +
+                  '<td>' +
+                  '<a href="'+item_base_url+'/details">' +
+                  item_name +
+                  '</a>' +
+                  '</td>' +
+                  '<td>' + item_value + '</td>' +
+                  '<td class="hidden-xs">' + item_time +'</td>' +
+                  '<td class="item-details">' +
+                  '<a href="'+item_base_url+'/stats" aria-label="Stats">' +
+                  '<span class="glyphicon glyphicon-stats" aria-hidden="true"></span>' +
+                  '</a>&nbsp;' +
+                  '<a href="'+item_base_url+'/details" aria-label="Details">' +
+                  '<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>' +
+                  '</a>' +
+                  '<a href="'+item_base_url+'/bookmark" aria-label="Bookmark">' +
+                  '<span class="glyphicon glyphicon-bookmark" aria-hidden="true"></span>' +
+                  '</a>' +
+                  '</td>' +
+                  '</tr>';
+            });
+
+            // Determine if a panel was collapsed before the refresh.
+            $('#items div.collapse').each(function(i, item) {
+                var id = item.id.substring(12);
+                collapsed[id] = $(this).hasClass('in');
+            });
+
+            // Remove all panels and build the page again.
+            $('#items').empty();
+
+            for (var key in categories) {
+                if (categories.hasOwnProperty(key)) {
                     $('#items').append(
                         '<div class="panel panel-default">' +
                         '<div class="panel-heading">' +
-                        note.subject +
+                        '<a data-toggle="collapse" data-target="#table-items-'+key+'">' +
+                        key +
+                        ' <span class="badge">'+alerts[key]+'</span>' +
+                        '</a>' +
                         '</div>' +
-                        '<div class="panel-body">' +
-                        '<p>' +
-                        note.message +
-                        '</p>' +
-                        '<div class="xs">' +
-                        ts.toLocaleString() +
-                        '</div>' +
+                        '<div class="collapse in" id="table-items-'+key+'">' +
+                        '<table class="table table-striped">' +
+                        '<thead>' +
+                        '<tr>' + 
+                        '<th>Name</th>' +
+                        '<th class="col-xs-2">Value</th>' +
+                        '<th class="col-sm-3 hidden-xs">Timestamp</th>' +
+                        '<th class="col-xs-2 col-sm-1"></th>' +
+                        '</tr>' +
+                        '</thead>' +
+                        '<tbody>' +
+                        '<tbody id="items-'+key+'">' +
+                        '</tbody>' +
+                        '</table>' +
                         '</div>' +
                         '</div>');
-                });
-                setTimeout(doPoll, 5000);
-            }
-        });
-    }
-    doPoll();
-}
-if (ARGUX_HOST_ACTION=='metrics') {
-    function doPoll() {
-        $.ajax({
-            url: ARGUX_BASE+"/rest/1.0/host/"+ARGUX_HOST+"?items=true",
-            type: "GET",
-            dataType: "json",
-            success: function(json) {
 
-                //var categories = {'global':'', 'net':'', 'storage':''};
-                //var alerts     = {'global':'', 'net':'', 'storage':''};
-                var categories = {}
-                var alerts     = {}
-                var collapsed  = {};
-
-
-                // Build the panel contents.
-                $.each(json.items, function(i, item) {
-
-                    var category = 'global';
-                    var item_name = item.key;
-                    var item_value = 'unknown';
-                    var item_time = '-';
-
-                    // Pick a category
-                    if (item.category !== null) { category = item.category;
+                    // Collapse panels that were collapsed before the refresh.
+                    if (collapsed[key] === false) {
+                        $('#table-items-'+key).removeClass('in');
                     }
 
-                    // If name exists, don't use the key.
-                    if (item.name !== null) {
-                        item_name = item.name;
-                    }
-
-                    //
-                    if (item.last_ts !== undefined) {
-                        item_time = item.last_ts;
-                    }
-
-                    if (item.last_val !== undefined) {
-                        item_value = item.last_val;
-                    }
-
-                    /* Initialise alerts to '' */
-                    if (alerts.hasOwnProperty(category) === false) {
-                        alerts[category] = '';
-                    }
-                    var item_base_url = 
-                        ARGUX_BASE +
-                        '/host/' +
-                        ARGUX_HOST +
-                        '/item/' +
-                        item.key
-
-                    categories[category]+='<tr>' +
-                      '<td>' +
-                      '<a href="'+item_base_url+'/details">' +
-                      item_name +
-                      '</a>' +
-                      '</td>' +
-                      '<td>' + item_value + '</td>' +
-                      '<td class="hidden-xs">' + item_time +'</td>' +
-                      '<td class="item-details">' +
-                      '<a href="'+item_base_url+'/stats" aria-label="Stats">' +
-                      '<span class="glyphicon glyphicon-stats" aria-hidden="true"></span>' +
-                      '</a>&nbsp;' +
-                      '<a href="'+item_base_url+'/details" aria-label="Details">' +
-                      '<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>' +
-                      '</a>' +
-                      '<a href="'+item_base_url+'/bookmark" aria-label="Bookmark">' +
-                      '<span class="glyphicon glyphicon-bookmark" aria-hidden="true"></span>' +
-                      '</a>' +
-                      '</td>' +
-                      '</tr>';
-                });
-
-                // Determine if a panel was collapsed before the refresh.
-                $('#items div.collapse').each(function(i, item) {
-                    var id = item.id.substring(12);
-                    collapsed[id] = $(this).hasClass('in');
-                });
-
-                // Remove all panels and build the page again.
-                $('#items').empty();
-
-                for (var key in categories) {
-                    if (categories.hasOwnProperty(key)) {
-                        $('#items').append(
-                            '<div class="panel panel-default">' +
-                            '<div class="panel-heading">' +
-                            '<a data-toggle="collapse" data-target="#table-items-'+key+'">' +
-                            key +
-                            ' <span class="badge">'+alerts[key]+'</span>' +
-                            '</a>' +
-                            '</div>' +
-                            '<div class="collapse in" id="table-items-'+key+'">' +
-                            '<table class="table table-striped">' +
-                            '<thead>' +
-                            '<tr>' + 
-                            '<th>Name</th>' +
-                            '<th class="col-xs-2">Value</th>' +
-                            '<th class="col-sm-3 hidden-xs">Timestamp</th>' +
-                            '<th class="col-xs-2 col-sm-1"></th>' +
-                            '</tr>' +
-                            '</thead>' +
-                            '<tbody>' +
-                            '<tbody id="items-'+key+'">' +
-                            '</tbody>' +
-                            '</table>' +
-                            '</div>' +
-                            '</div>');
-
-                        // Collapse panels that were collapsed before the refresh.
-                        if (collapsed[key] === false) {
-                            $('#table-items-'+key).removeClass('in');
-                        }
-
-                        // Add the panel-content.
-                        $('#items-'+key).append(
-                            categories[key]
-                            );
-                    }
+                    // Add the panel-content.
+                    $('#items-'+key).append(
+                        categories[key]
+                        );
                 }
+            }
 
-                /*
-                $("#hosts tr").click(function() {
-                    var href = $(this).find("a").attr("href");
-                    if (href) {
-                        window.location = href;
-                    }
+            setTimeout(pollMetrics, 5000);
+        }
+    });
+
+}
+
+$(function() {
+    if (ARGUX_HOST_ACTION==='notes') {
+        $('#notes-form').submit(function(event) {
+
+            var subject = $('#note-subject').val();
+            var message = $('#note-body').val();
+
+            if (( subject !== "") &&
+                ( message !== "")) {
+                // Use JSON.stringify to properly escape the message and subject
+                // for use in a JSON message envelope
+                $.ajax({
+                    type: 'POST',
+                    url:  ARGUX_BASE+'/rest/1.0/note',
+                    dataType: 'json',
+                    data: '{'+
+                          '"host": "'+ARGUX_HOST+'",' +
+                          '"message": '+JSON.stringify(message)+',' +
+                          '"subject": '+JSON.stringify(subject)+
+                          '}'
                 });
-                */
-                setTimeout(doPoll, 5000);
             }
         });
+
+        pollNotes();
     }
-    doPoll();
-}
+
+    if (ARGUX_HOST_ACTION==='metrics') {
+        pollMetrics();
+    }
 });
 
 // Show/hide details below host header.
--- scripts/create_items.sh
+++ scripts/create_items.sh
@@ -1,5 +1,6 @@
 
 SERVER=http://localhost:6543
+ARGUX_BASE=/
 REST_URI=rest/1.0
 HOST_URI=host
 
@@ -27,8 +28,6 @@ curl -X POST \
         \"type\":\"float\"
         }" \
     $SERVER/$REST_URI/$HOST_URI/$HOST_NAME/item/cpu.load.avg\\\[1\\\]
-
-exit 0
 
 curl -X POST \
      -H "Content-Type: application/json" \
