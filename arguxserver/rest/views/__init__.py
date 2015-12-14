e757a694d5677d190e1fcaa335fc35e3dbf33e6a
Stephan Arts <stephan@xfce.org>
2015-12-14 22:42:07 +0100

    Improve style of abstract RestView class

--- arguxserver/rest/views/__init__.py
+++ arguxserver/rest/views/__init__.py
@@ -1,7 +1,13 @@
-# package
+"""Abstract RestView class."""
 
-class RestView:
+class RestView(object):
+
+    """RestView class.
+
+    Contains dao and request object.
+    """
 
     def __init__(self, request):
+        """Initialise RestView."""
         self.request = request
         self.dao = request.registry.settings['dao']
