bc3877bda7d5f7b4137b713a0c13c1e3a3a3021b
Stephan Arts <stephan@xfce.org>
2016-01-20 09:25:15 +0100

    Improve regexp, add username_expr

--- arguxserver/util.py
+++ arguxserver/util.py
@@ -6,13 +6,23 @@ TIME_OFFSET_EXPR = re.compile(r"([-+])([0-9]+)([mhd])")
 
 TIMESPAN_EXPR = re.compile(r"([0-9]+)([mhdM])")
 
+"""Regular expression matching trigger names.
+
+selector(<number>) compare [number]
+
+These trigger rules only make sense for integer
+and floating-point numbers.
+"""
 TRIGGER_EXPR = re.compile(
     r"([a-z]+)"
-     "\(([0-9]*)\)"
-     "[ ]*"
-     "(>|<|>=|<=|==|!=)"
-     "[ ]*"
-     "([-]?(?:[0-9]+(?:[\.,][0-9]+)?))")
+    r"\(([0-9]*)\)"
+    r"[ ]*"
+    r"(>|<|>=|<=|==|!=)"
+    r"[ ]*"
+    r"([-]?(?:[0-9]+(?:[\.,][0-9]+)?))")
 
 DATE_FMT = "%Y-%m-%dT%H:%M:%S"
 
+USERNAME_EXPR = re.compile(
+    r"((?:([A-Z])+[/])?"
+    r"([a-z]+))")
