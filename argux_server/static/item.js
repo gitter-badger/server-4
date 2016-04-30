7a36c80c3cdf87e4fd7357c3893fa1e8b4bdf37b
Stephan Arts <stephan@xfce.org>
2016-04-30 23:55:00 +0200

    remove obsolete js

--- argux_server/static/item.js
+++ argux_server/static/item.js
@@ -10,31 +10,6 @@
 var chart_start_time = null;
 var chart_end_time = 'now';
 
-Chart.controllers.line_category = Chart.DatasetController.extend({
-    // Create elements for each piece of data in the dataset. Store elements in an array on the dataset as dataset.metaData
-    addElements: function() {},
-
-    // Create a single element for the data at the given index and reset its state
-    addElementAndReset: function(index) {},
-
-    // Draw the representation of the dataset
-    // @param ease : if specified, this number represents how far to transition elements. See the implementation of draw() in any of the provided controllers to see how this should be used
-    draw: function(ease) {},
-
-    // Remove hover styling from the given element
-    removeHoverStyle: function(element) {},
-
-    // Add hover styling to the given element
-    setHoverStyle: function(element) {},
-
-    // Update the elements in response to new data
-    // @param reset : if true, put the elements into a reset state so they can animate to their final values
-    update: function(reset) {},
-});
-
-
-Chart.defaults.global.responsive = true;
-
 var dataset_avg = {
     label: 'Average',
     borderWidth: 1,
